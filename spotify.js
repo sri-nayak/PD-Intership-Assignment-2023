a835be44bdbd38c9907440245f7b0846d67a419b
sri-nayak <sridhara.nayak@prodevans.com>
2023-09-21 14:54:11 +0530

    further changes

--- spotify.js
+++ spotify.js
@@ -1,15 +1,35 @@
-const token = 'BQCpffZgHxLdzpnWeR6lt5bnV8iwxBypqgyZ37dvkOMaIiNRylhZeBsIANNfkbkAl0-PN5qPd7jPdZlC-f6lC0QYMF60IYdUEwRqcan9nuM7xE6TtuPRkkbRTdhAi0XkxRVt5oTGzzROrwSp13WYk2O2uhZBXXveH5D8QQr_1Cw6BkfvfoO4plqUNSGN_ija9gRCjuggjUDyR8q8I7BhqRoauSLzMActwT7FOeOrZQO6F183PUvtxagpVJSwtiBSHxlmbxt-jSHMvi3KIYBxZyKg';
+var client_id = 'e0ed96518dc04c7c90141cb4b87c70f7';
+var client_secret='cb05a583cb274e4e942f42ec447a6c60'
 var artistList=['4YRxDV8wJFPHPTeXepOstw','0TnOYISbd1XYRBk9myaseg','1mUl05hT77FrwVFW51wOlr','0xWGBvlwCBHAgXyR7TmPwp','2CIMQHirSU0MQqyYHq0eOx','57dN52uHvrHOxijzpIgu3E','1vCWHaC5f2uS3yhpwWbIA6','7o7doCwqft91WC690aglWC']
+
+
+async function logIn() {
+  var cred=btoa(`${client_id}:${client_secret}`)
+  let data=await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+        'Authorization': `Basic ${cred}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials'
+});
+ var dat=await data.json()
+ console.log(dat.access_token);
+  return dat.access_token;
+}
+var token=logIn()
+console.log(typeof(token));
 const url = `https://api.spotify.com/v1/artists?ids=${artistList}`
-const request = new Request(
-    url,{
-        headers:{
-            'Authorization': `Bearer ${token}`
-        }
-    },'GET')
+
 
 async function getData(){
     try{
+      const request = new Request(
+        url,{
+            headers:{
+                'Authorization': `Bearer ${await token}`
+            }
+        },'GET')
         const response = await fetch(request) 
         const data = await response.json()
         if(response.status == 200) {
@@ -26,29 +46,27 @@ getData()
     let doc=document.getElementById("name")
     const data=num
    for(let i in num){
-    console.log("times")
-    doc.insertAdjacentHTML('beforeend',`<div class="container"><div class="main">
-    <div class="head">
-      <div class="row">
-        <div class="col-md-6 image">
-          <img
+    doc.insertAdjacentHTML('beforeend',
+    `<div class="container">
+      <div class="main">
+        <div class="image">
+           <img
             src=${num[i].images[0].url}
             alt="Image of Artist"
           />
         </div>
-        <div class="col-md-6 paragraph">
-          <p>Name: ${num[i].name}</p><br />
+      <div class="body">
+        <div class=" paragraph">
+          <h1>${num[i].name}</h1><br />
           <p>Follower: ${num[i].followers.total}</p><br />
           <p>Popularity: ${num[i].popularity}</p><br />
           <p>Music Kind:${num[i].genres?.find(element => {return element})}
           </p><br />
         </div>
-      </div></div>`)
+      </div>
+      </div>
+    </div>`)
     console.log(num[i])
-   }
-//    for()
-//    doc.innerHTML=temp;
-
+  }
 }
-renderData()
 
