a7c85b5e5ef39c124b453b39a4ef184ca5c18c81
sri-nayak <sridhara.nayak@prodevans.com>
2023-09-22 17:19:57 +0530

    final submission

--- JS/dummy.js
+++ JS/dummy.js
@@ -4,12 +4,20 @@ var key='8c41ffef132b3af1a9f444d767a390c5'
 async function weather() {
     var loc='delhi'
     var result=await getData(loc)
-    console.log(result.status);
-    console.log(result.main.temp)
+    console.log(result.tempe);
+    // console.log(result.mai)
 }
 async function getData(location) {
-    var result=await fetch(url+`?q=${location}&appid=${key}`)
-    console.log(result.status);
-    return result.json()
+    var data={status:null,error:null,tempe:null}
+    try{
+        var result=await fetch(url+`?q=${location}&appid=${key}`)
+        data.status=result.status
+        data.error=null
+        data.tempe=await fetch(url+`?q=${location}&appid=${key}`)
+        return data
+    }catch(error){
+        data.error=result.error
+        console.log('Server error')
+    }
 }
 weather()
--- JS/index.js
+++ JS/index.js
@@ -4,25 +4,44 @@ var key='8c41ffef132b3af1a9f444d767a390c5'
 async function weather() {
     let temp;
     let search=document.getElementById("search-box").value.toString().toLowerCase()
+    let image=document.getElementById('weather-icon')
     let weather=document.getElementById("weather-detail")
     console.log(search);
     let loc=document.getElementById("Location")
     if (search.length==0) {
         loc.innerHTML="Enter a location"
         loc.style.color='red'
-        loc.style.visibility='visible'   
-    } else {
-        let response=await getData(search)
-        temp=response.main.temp
-        console.log(typeof(temp));
         loc.style.visibility='visible'
-        weather.style.visibility='visible'
-        loc.innerHTML=search
-        weather.innerHTML=`${temp}\u00B0k/${Number((temp-273).toFixed(1))}\u00B0C`
-        console.log(response.main.temp.toString()+"/"+parseInt(response.main.temp)-273.15);
+        weather.style.visibility='hidden'   
+    } else {
+        try{
+            var response=await fetch(url+`?q=${search}&appid=${key}`)
+                if (response.status==200) {
+                    let result= await response.json()
+                    console.log(result)
+                    temp=result.main.temp
+                    console.log(typeof(temp));
+                    loc.style.color="black"
+                    loc.style.visibility='visible'
+                    weather.style.visibility='visible'
+                    loc.innerHTML=search
+                    weather.innerHTML=`${temp}\u00B0k/${Number((temp-273).toFixed(1))}\u00B0C`  
+                    image.src = `http://openweathermap.org/img/w/${result.weather[0].icon}.png`; 
+                } else {
+                    image.style.visibility="hidden"
+                 weather.style.visibility='hidden'
+                 loc.style.visibility='visible' 
+                 loc.style.color="red"
+                 loc.innerHTML=`Weather detail for specified place couldn't find`
+                 
+                } 
+        }catch(error){
+            image.style.visibility="hidden"
+            weather.style.visibility='visible'
+            loc.innerHTML='Internal Server Error'
+            console.log('Server error')
+        }
+        // let response=await getData(search)
+        // console.log(response.error)
     }  
-}
-async function getData(location) {
-    var result=await fetch(url+`?q=${location}&appid=${key}`)
-    return result.json()
 }
